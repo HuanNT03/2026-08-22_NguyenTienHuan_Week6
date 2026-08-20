9200ada0a84b4a7cb25056131c2c7bab2a583952
HuanNT03 <21021500@vnu.edu.vn>
2026-08-20 11:42:50 +0700

    refactor(build): integrate UnifiedMarkdownParser into build pipeline and rebuild KB

--- src/retrieval/build.py
+++ src/retrieval/build.py
@@ -8,31 +8,25 @@ from pathlib import Path
 
 from src.retrieval.config import (
     ASVS_RAW_DIR,
-    CHEATSHEETS_RAW_DIR,
-    CODEQL_RAW_DIR,
     CWE_RAW_PATHS,
     DOCUMENTS_PATH,
     EXAMPLES_DIR,
+    KNOWLEDGE_BASE_DIR,
     MANIFEST_PATH,
-    OWASP_TOP_TEN_DIR,
     QDRANT_COLLECTION_NAME,
     QDRANT_STORAGE_DIR,
     SCHEMA_PATH,
     SCHEMA_VERSION,
     SEMGREP_RAW_DIR,
-    ZAP_RAW_DIR,
 )
 from src.retrieval.embeddings.client import EmbeddingClient
 from src.retrieval.exceptions import DuplicateDocumentIdError, SourceValidationError
 from src.retrieval.models import KnowledgeDocument
 from src.retrieval.parsers.asvs_parser import parse_asvs_csv
-from src.retrieval.parsers.cheatsheet_parser import parse_cheatsheet_directory
 from src.retrieval.parsers.cwe_parser import CweParseResult, parse_cwe_views
 from src.retrieval.parsers.example_parser import parse_example_directory
-from src.retrieval.parsers.markdown_doc_parser import parse_generic_markdown_directory
-from src.retrieval.parsers.owasp_parser import parse_owasp_directory
 from src.retrieval.parsers.semgrep_rule_parser import parse_semgrep_rules_directory
-from src.retrieval.parsers.zap_alert_parser import parse_zap_alerts_directory
+from src.retrieval.parsers.unified_markdown_parser import parse_all_markdown_sources
 from src.retrieval.storage.jsonl_store import atomic_write_bytes, serialize_documents, sha256_bytes
 from src.retrieval.validation import build_knowledge_validator, load_knowledge_schema, validate_document
 from src.retrieval.vector.qdrant_store import QdrantVectorStore
@@ -91,14 +85,7 @@ def collect_documents() -> CollectionResult:
         validate_document(doc, validator)
         registry.add(doc)
 
-    # 2. OWASP Top 10 (Multi-version: 2025, 2021, 2017)
-    owasp_docs, owasp_warnings = parse_owasp_directory(OWASP_TOP_TEN_DIR)
-    warnings.extend(owasp_warnings)
-    for doc in owasp_docs:
-        validate_document(doc, validator)
-        registry.add(doc)
-
-    # 3. OWASP ASVS Requirements
+    # 2. OWASP ASVS Requirements (CSV)
     asvs_csv = ASVS_RAW_DIR / "OWASP_Application_Security_Verification_Standard_5.0.0_en.csv"
     if asvs_csv.is_file():
         asvs_docs = parse_asvs_csv(asvs_csv)
@@ -106,14 +93,7 @@ def collect_documents() -> CollectionResult:
             validate_document(doc, validator)
             registry.add(doc)
 
-    # 4. OWASP Cheat Sheets
-    if CHEATSHEETS_RAW_DIR.is_dir():
-        cheatsheets = parse_cheatsheet_directory(CHEATSHEETS_RAW_DIR)
-        for doc in cheatsheets:
-            validate_document(doc, validator)
-            registry.add(doc)
-
-    # 5. Semgrep Rules
+    # 3. Semgrep Rules (YAML)
     semgrep_rules_dir = SEMGREP_RAW_DIR / "rules"
     if semgrep_rules_dir.is_dir():
         rules = parse_semgrep_rules_directory(semgrep_rules_dir)
@@ -121,73 +101,20 @@ def collect_documents() -> CollectionResult:
             validate_document(doc, validator)
             registry.add(doc)
 
-    # 6. OWASP ZAP Alerts
-    zap_alerts_dir = ZAP_RAW_DIR / "alerts"
-    if zap_alerts_dir.is_dir():
-        alerts = parse_zap_alerts_directory(zap_alerts_dir)
-        for doc in alerts:
-            validate_document(doc, validator)
-            registry.add(doc)
-
-    # 7. CodeQL Documentation
-    if CODEQL_RAW_DIR.is_dir():
-        codeql_docs = parse_generic_markdown_directory(
-            CODEQL_RAW_DIR,
-            source_name="CodeQL Documentation",
-            doc_type="scanner_document",
-            doc_id_prefix="codeql-doc",
-            extra_tags=["codeql", "sast", "data-flow"],
-        )
-        for doc in codeql_docs:
-            validate_document(doc, validator)
-            registry.add(doc)
-
-    # 8. Semgrep Vulnerability Guides & Docs
-    semgrep_vulns_dir = SEMGREP_RAW_DIR / "vulnerabilities"
-    if semgrep_vulns_dir.is_dir():
-        vuln_guides = parse_generic_markdown_directory(
-            semgrep_vulns_dir,
-            source_name="Semgrep Vulnerability Guides",
-            doc_type="scanner_document",
-            doc_id_prefix="semgrep-vuln",
-            extra_tags=["semgrep", "vulnerability-guide"],
-            recursive=True,
-        )
-        for doc in vuln_guides:
-            validate_document(doc, validator)
-            registry.add(doc)
-
-    semgrep_docs_dir = SEMGREP_RAW_DIR / "docs"
-    if semgrep_docs_dir.is_dir():
-        semgrep_docs = parse_generic_markdown_directory(
-            semgrep_docs_dir,
-            source_name="Semgrep Documentation",
-            doc_type="scanner_document",
-            doc_id_prefix="semgrep-doc",
-            extra_tags=["semgrep", "docs"],
-        )
-        for doc in semgrep_docs:
-            validate_document(doc, validator)
-            registry.add(doc)
-
-    # 9. OWASP ZAP Docker Guides
-    zap_docker_dir = ZAP_RAW_DIR / "docker"
-    if zap_docker_dir.is_dir():
-        zap_docker_docs = parse_generic_markdown_directory(
-            zap_docker_dir,
-            source_name="OWASP ZAP Docker Guides",
-            doc_type="scanner_document",
-            doc_id_prefix="zap-docker",
-            extra_tags=["zap", "docker"],
-        )
-        for doc in zap_docker_docs:
-            validate_document(doc, validator)
-            registry.add(doc)
-
-    # 10. Curated Vulnerability Examples
+    # 4. Curated Vulnerability Examples (YAML)
     if EXAMPLES_DIR.is_dir():
         examples = parse_example_directory(EXAMPLES_DIR)
         for doc in examples:
+            validate_document(doc, validator)
+            registry.add(doc)
+
+    # 5. Unified Markdown Ingestion (100% of all Markdown in raw/)
+    # Covers OWASP Top 10 (2017/2021/2025), ZAP Alerts & Guides, Cheatsheets, CodeQL, Semgrep docs/vulns, ASVS docs
+    raw_root = KNOWLEDGE_BASE_DIR / "raw"
+    if raw_root.is_dir():
+        md_docs, md_warnings = parse_all_markdown_sources(raw_root)
+        warnings.extend(md_warnings)
+        for doc in md_docs:
             validate_document(doc, validator)
             registry.add(doc)
 
